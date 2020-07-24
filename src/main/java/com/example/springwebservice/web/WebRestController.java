2e49f314daccf3a3c23617ed1113db2f56fcaa07
hckim96 <hyeoncheolkim96@gmail.com>
2020-07-24 11:11:21 +0900

    PostsSave

--- src/main/java/com/example/springwebservice/web/WebRestController.java
+++ src/main/java/com/example/springwebservice/web/WebRestController.java
@@ -1,14 +1,26 @@
 package com.example.springwebservice.web;
 
+import com.example.springwebservice.domain.posts.PostsRepository;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.AllArgsConstructor;
+
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+    private PostsRepository postsRepository;
 
     @GetMapping("/hello")
     public String hello() {
         return "hello";
     }
 
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto) {
+        postsRepository.save(dto.toEntity());
+    }
 }
--- src/test/java/com/example/springwebservice/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/example/springwebservice/domain/posts/PostsRepositoryTest.java
@@ -4,13 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-// import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 public class PostsRepositoryTest {
     @Autowired
