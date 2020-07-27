46d7e24ed221b2cd00191466dfaa05edec65b37e
hckim96 <hyeoncheolkim96@gmail.com>
2020-07-28 01:03:07 +0900

    add path for EC2

--- src/main/java/com/example/springwebservice/SpringWebserviceApplication.java
+++ src/main/java/com/example/springwebservice/SpringWebserviceApplication.java
@@ -9,7 +9,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class SpringWebserviceApplication {
 	public static final String APPLICATION_LOCATIONS = "spring.config.location=" + "classpath:application.yml,"
-			+ "C:/Users/user/app/config/springboot-webservice/real-application.yml";
+			+ "C:/Users/user/app/config/springboot-webservice/real-application.yml," // for window
+			+ "/Users/hc/app/config/springboot-webservice/real-application.yml," // for mac
+			+ "/home/ec2-user/app/config/springboot-webservice/real-application.yml"; // for ec2
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(SpringWebserviceApplication.class).properties(APPLICATION_LOCATIONS).run(args);
